a709b025fe480aca64d158efe70708306370188a
JeDTr <tvthong1995@gmail.com>
2020-01-12 23:56:58 +0700

    Done React audio player

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import TimeSlider from "react-input-slider";
 
-function App() {
+import "./App.scss";
+import TetImg from "./images/tet.jpg";
+import PrevIcon from "./icons/PrevIcon";
+import NextIcon from "./icons/NextIcon";
+import PauseIcon from "./icons/PauseIcon";
+import PlayIcon from "./icons/PlayIcon";
+import audios from "./audios";
+
+const App = () => {
+  const audioRef = useRef();
+  const [audioIndex, setAudioIndex] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [isPlay, setPlay] = useState(false);
+
+  const handleLoadedData = () => {
+    setDuration(audioRef.current.duration);
+    if (isPlay) audioRef.current.play();
+  };
+
+  const handlePausePlayClick = () => {
+    if (isPlay) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setPlay(!isPlay);
+  };
+
+  const handleTimeSliderChange = ({ x }) => {
+    audioRef.current.currentTime = x;
+    setCurrentTime(x);
+
+    if (!isPlay) {
+      setPlay(true);
+      audioRef.current.play();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <img className="Song-Thumbnail" src={TetImg} alt="tet" />
+      <h2 className="Song-Title">{audios[audioIndex].title}</h2>
+      <p className="Singer">{audios[audioIndex].artist}</p>
+      <div className="Control-Button-Group">
+        <div
+          className="Prev-Button"
+          onClick={() => setAudioIndex((audioIndex - 1) % audios.length)}
         >
-          Learn React
-        </a>
-      </header>
+          <PrevIcon />
+        </div>
+        <div className="Pause-Play-Button" onClick={handlePausePlayClick}>
+          {isPlay ? <PauseIcon /> : <PlayIcon />}
+        </div>
+        <div
+          className="Next-Button"
+          onClick={() => setAudioIndex((audioIndex + 1) % audios.length)}
+        >
+          <NextIcon />
+        </div>
+      </div>
+      <TimeSlider
+        axis="x"
+        xmax={duration}
+        x={currentTime}
+        onChange={handleTimeSliderChange}
+        styles={{
+          track: {
+            backgroundColor: "#e3e3e3",
+            height: "2px",
+          },
+          active: {
+            backgroundColor: "#333",
+            height: "2px",
+          },
+          thumb: {
+            marginTop: "-3px",
+            width: "8px",
+            height: "8px",
+            backgroundColor: "#333",
+            borderRadius: 0,
+          },
+        }}
+      />
+      <audio
+        ref={audioRef}
+        src={audios[audioIndex].src}
+        onLoadedData={handleLoadedData}
+        onTimeUpdate={() => setCurrentTime(audioRef.current.currentTime)}
+        onEnded={() => setPlay(false)}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/audios/index.js
@@ -0,0 +1,23 @@
+import XuanOiOLaiChoi from "./XuanOiOLaiChoi.mp3";
+import NgayXuanLongPhungSumVay from "./NgayXuanLongPhungSumVay.mp3";
+import DoanXuanCa from "./DoanXuanCa.mp3";
+
+const audios = [
+  {
+    src: XuanOiOLaiChoi,
+    title: "Xuân Ơi Ở Lại Chơi",
+    artist: "Tăng Nhật Tuệ - Hoàng Yến Chibi - Tino",
+  },
+  {
+    src: NgayXuanLongPhungSumVay,
+    title: "Ngày Xuân Long Phụng Sum Vầy",
+    artist: "Bích Phương",
+  },
+  {
+    src: DoanXuanCa,
+    title: "Đoản Xuân Ca",
+    artist: "Bích Phương",
+  },
+];
+
+export default audios;
